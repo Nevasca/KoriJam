28eb926e004bdba9435a3d82a90c48a098a47c14
Bruno Sanches <bruno_sanches15@hotmail.com>
2021-05-02 18:20:00 -0300

    Starting MainScene

--- Assets/Scripts/Environment/MovingPlataform.cs
+++ Assets/Scripts/Environment/MovingPlataform.cs
@@ -9,7 +9,8 @@ public class MovingPlataform : MonoBehaviour, IActivable
     [SerializeField] private Transform[] points;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float waitTime = 2f;
-    [SerializeField] private PlataformState currentState;
+    [SerializeField] private PlataformState currentState;    
+    [SerializeField] private bool disableAfterPoints = false;
 
     private Rigidbody rb;
     private int indexPoint = 0;
@@ -42,8 +43,15 @@ public class MovingPlataform : MonoBehaviour, IActivable
         distanceToDestination = points[indexPoint].position - transform.position;
         if(distanceToDestination.sqrMagnitude < 0.001f)
         {
-            currentState = PlataformState.Waiting;
-            indexPoint = (indexPoint + 1) % points.Length;
+            if(disableAfterPoints && indexPoint == points.Length - 1)
+            {
+                currentState = PlataformState.Disabled;
+            }
+            else
+            {
+                currentState = PlataformState.Waiting;
+                indexPoint = (indexPoint + 1) % points.Length;
+            }
         }
     }
 
